9b7bca5ec1c13f5693b162eb3ade8ce68eeca640
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2023-10-08 23:50:31 +0545

    changes

--- src/redux/slices/counter/index.jsx
+++ src/redux/slices/counter/index.jsx
@@ -1,16 +1,13 @@
-import {createSlice} from '@reduxjs/toolkit'
-
+import { createSlice } from "@reduxjs/toolkit";
 
 export const counterSlice = createSlice({
-initialState :0,
-name:'counter',
-reducers:{
-     increament:(state)=> state+1,   
-     decreament:(state) => state-1
-}
+  initialState: 0,
+  name: "counter",
+  reducers: {
+    increament: (state) => state + 1,
+    decreament: (state) => state - 1,
+  },
+});
 
-})
-
-
-export const {decreament,increament} =counterSlice.actions
+export const { decreament, increament } = counterSlice.actions;
 export default counterSlice.reducer;
